7b58c7998c37b9bbe7d942e33dd44ab496589e1c
minjae Kim <cmg981548@gmail.com>
2024-04-03 21:58:52 +0900

    feat: [express-passport] bcrypt를 이용한 비밀번호

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = mongoose.Schema({
   email: {
@@ -18,8 +19,32 @@ const userSchema = mongoose.Schema({
 });
 // userSchema라는 변수에 mongoose Schema를 생성한다.
 
-userSchema.methods.comparePassword = async function (plainPassword) {
-  return plainPassword === this.password;
+const saltRounds = 10;
+userSchema.pre("save", function (next) {
+  let user = this;
+  if (user.isModified("password")) {
+    // salt 생성
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword) {
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(plainPassword, this.password, (err, isMatch) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(isMatch);
+    });
+  });
 };
 
 const User = mongoose.model("User", userSchema);
