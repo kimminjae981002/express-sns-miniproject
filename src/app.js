c3e9a08d5ce915955f1ceaae9fe442d4e40b5e35
minjae Kim <cmg981548@gmail.com>
2024-04-04 15:56:37 +0900

    feat: [express-passport] router 기능 이용해서 파일 분리하기

--- src/app.js
+++ src/app.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const passport = require("passport");
-const ejs = require("ejs");
 const path = require("path");
 const cookieSession = require("cookie-session");
 require("dotenv").config();
 
-const {
-  checkAuthenticated,
-  checkNotAuthenticated,
-} = require("./middlewares/auth");
+const mainRouter = require("./routes/main.router");
+const usersRouter = require("./routes/user.router");
 
 const app = express();
 
@@ -55,77 +52,8 @@ mongoose
     console.log(err);
   });
 
-app.get("/", checkAuthenticated, (req, res) => {
-  res.render("index");
-});
-
-app.get("/login", checkNotAuthenticated, (req, res) => {
-  res.render("login");
-});
-
-// /login미들웨어 안에 authenticate 미들웨어가 또 있다.
-// 그래서 authenticate 미들웨어로 가기 위해서는
-// 66번째줄에 즉시 함수 실행을 해주면 된다.
-app.post("/login", (req, res, next) => {
-  passport.authenticate("local", (err, user, info) => {
-    // passport.js에서 done
-    if (err) {
-      return next(err);
-    }
-
-    if (!user) {
-      console.log(err);
-      return res.json({ msg: info });
-    }
-
-    // passport에서 제공하는 함수 logIn
-    req.logIn(user, function (err) {
-      if (err) {
-        return next(err);
-      }
-      res.redirect("/");
-    });
-  })(req, res, next);
-});
-
-app.get("/auth/google", passport.authenticate("google"));
-app.get(
-  "/auth/google/callback",
-  passport.authenticate("google", {
-    successReturnToOrRedirect: "/",
-    failureRedirect: "/login",
-  })
-);
-
-app.post("/logout", (req, res, next) => {
-  req.logOut((err) => {
-    if (err) {
-      return next(err);
-    }
-    res.redirect("/login");
-  });
-});
-
-app.get("/signup", checkNotAuthenticated, (req, res) => {
-  res.render("signup");
-});
-
-const User = require("./models/user.model");
-
-app.post("/signup", async (req, res) => {
-  // user 객체 생성
-  const user = new User(req.body);
-
-  // db에 user 저장
-  try {
-    await user.save();
-    return res.status(200).json({
-      success: true,
-    });
-  } catch (error) {
-    console.error(error);
-  }
-});
+app.use("/", mainRouter);
+app.use("/", usersRouter);
 
 app.listen("3000", (req, res) => {
   console.log("listening 3000");
---
+++ src/routes/user.router.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const {
+  checkNotAuthenticated,
+  checkAuthenticated,
+} = require("../middlewares/auth");
+const passport = require("passport");
+
+const usersRouter = express.Router();
+
+// /login미들웨어 안에 authenticate 미들웨어가 또 있다.
+// 그래서 authenticate 미들웨어로 가기 위해서는
+// 66번째줄에 즉시 함수 실행을 해주면 된다.
+usersRouter.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    // passport.js에서 done
+    if (err) {
+      return next(err);
+    }
+
+    if (!user) {
+      console.log(err);
+      return res.json({ msg: info });
+    }
+
+    // passport에서 제공하는 함수 logIn
+    req.logIn(user, function (err) {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/");
+    });
+  })(req, res, next);
+});
+
+usersRouter.post("/logout", (req, res, next) => {
+  req.logOut((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/login");
+  });
+});
+
+const User = require("../models/user.model");
+
+usersRouter.post("/signup", async (req, res) => {
+  // user 객체 생성
+  const user = new User(req.body);
+
+  // db에 user 저장
+  try {
+    await user.save();
+    return res.status(200).json({
+      success: true,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+usersRouter.get("/auth/google", passport.authenticate("google"));
+usersRouter.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successReturnToOrRedirect: "/",
+    failureRedirect: "/login",
+  })
+);
+
+module.exports = usersRouter;
