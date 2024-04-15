46169ab60375c70e1d18b729e1dfc7f5bca1685d
minjae Kim <cmg981548@gmail.com>
2024-04-15 21:14:38 +0900

    feat: [main] Post UI ejs 작성 및 ejs에 데이터 보내주기

--- src/routes/posts.router.js
+++ src/routes/posts.router.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const { checkAuthenticated } = require("../middlewares/auth");
+const Post = require('../models/posts.model')
 
+// Post DB에 있는 모든 것들을 찾고 comments 데이터도 보여주고 실행해라
+// posts에는 posts 데이터를 가져오고 currentUser에는 현재 로그인 된 유저를 넣어준다.
+// ejs에서 사용가능하다.
 router.get("/", checkAuthenticated, (req, res) => {
-  res.render("posts/index");
+  Post.find().populate('comments').sort({ createdAt: -1 }).exec()
+    .then(posts => {
+      res.render("posts/index", {
+        posts: posts,
+        currentUser: req.user
+      })
+    }).catch(err => {
+    console.log(err)
+  })
+
 });
 
 module.exports = router;
