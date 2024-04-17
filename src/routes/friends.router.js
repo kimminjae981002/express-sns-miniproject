5bfe3a0b4e3e38ca952a2aaf6b328931aa17d5ad
minjae Kim <cmg981548@gmail.com>
2024-04-17 21:54:30 +0900

    feat: [main] 친구 신청 로직

--- src/routes/friends.router.js
+++ src/routes/friends.router.js
@@ -16,4 +16,18 @@ router.get('/',checkAuthenticated, async (req, res) => {
     })
 })
 
+// 친구 요청
+router.put('/:id/add-friend', checkAuthenticated, async (req, res) => {
+    // 몽고db 연산자 $push는 항상 값을 추가한다. 이미 존재 했어도
+    // $addToSet은 중복된 경우 하지 않는다.
+    await User.findByIdAndUpdate(req.params.id,
+        {$addToSet: { friendsRequests: req.user._id }})
+       .then(async (user) => {
+            req.flash('success', '친구 추가에 성공했습니다.')
+        }).catch((err) => {
+            console.log(err)
+            req.flash('error', '친구 추가에 실패했습니다.')
+        })
+})
+
 module.exports = router;
