29d856648abd3e1e1256b46cd885effd864e8f09
minjae Kim <cmg981548@gmail.com>
2024-04-17 22:30:43 +0900

    feat: [main] 친구 요청 / 수락 / 요청 취소 및 거절 / 친구 취소 로직

--- src/routes/friends.router.js
+++ src/routes/friends.router.js
@@ -23,11 +23,66 @@ router.put('/:id/add-friend', checkAuthenticated, async (req, res) => {
     await User.findByIdAndUpdate(req.params.id,
         {$addToSet: { friendsRequests: req.user._id }})
        .then(async (user) => {
-            req.flash('success', '친구 추가에 성공했습니다.')
+           req.flash('success', '친구 추가에 성공했습니다.')
+           res.redirect('back')
         }).catch((err) => {
             console.log(err)
             req.flash('error', '친구 추가에 실패했습니다.')
+            res.redirect('back')
         })
 })
+
+// 친구 요청 취소
+router.put('/:id/remove-friend-request/:currentUserId', checkAuthenticated, async (req, res) => {
+    // $pull 몽고디비 배열에서 제거
+    await User.findByIdAndUpdate(req.params.id, {
+        $pull: {friendsRequests: req.params.currentUserId}
+    }).then((user) => {
+        req.flash('success', '친구 요청이 취소되었습니다.')
+        res.redirect('back')
+    }).catch((err) => {
+        console.log(err)
+        req.flash('error', '친구 요청 취소 중 에러가 발생했습니다.')
+        res.redirect('back')
+    })
+})
+
+// 친구 요청 수락
+router.put('/:id/accept-friend-request', checkAuthenticated, async (req, res) => {
+    
+    await User.findByIdAndUpdate(req.user._id, {
+        $addToSet: { friends: req.params.id },
+        $pull: {friendsRequests: req.params.id}
+    })
+    await User.findByIdAndUpdate(req.params.id, {
+        $addToSet: { friends: req.user._id },
+    })   
+        .then(() => {
+        req.flash('success', '친구 요청이 수락되었습니다.')
+        res.redirect('back')
+    }).catch(() => {
+        req.flash('error', '친구 요청이 수락 중 에러가 발생했습니다..')
+        res.redirect('back')
+    })
+})
+
+// 친구 취소
+router.put('/:id/remove-friend', checkAuthenticated, async (req, res) => {
+    
+    await User.findByIdAndUpdate(req.user._id, {
+        $pull: {friends: req.params.id}
+    })
+    await User.findByIdAndUpdate(req.params.id, {
+        $pull: { friends: req.user._id },
+    })   
+        .then(() => {
+        req.flash('success', '친구 삭제되었습니다.')
+        res.redirect('back')
+    }).catch(() => {
+        req.flash('error', '친구 삭제 중 에러가 발생했습니다..')
+        res.redirect('back')
+    })
+})
+
 
 module.exports = router;
